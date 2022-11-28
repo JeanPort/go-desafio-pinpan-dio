6801a6de667ba5ff6fd8ce8aec083548e1d465eb
Jean Port <pierreanatc522@gmail.com>
2022-11-28 18:26:22 -0300

    firt commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	ebulicaoK := 373
+	ebulicaoC := ebulicaoK - 273
+
+	fmt.Println("A temperatura de ebulição da água em ºF igual", ebulicaoK)
+	fmt.Println("A temperatura de ebulição da água em ºC =", ebulicaoC)
+
+}
